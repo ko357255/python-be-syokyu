4bc9ebd759712e698f2b8c60da0f51077a4b01b3
ko357255 <mikanace@outlook.jp>
2025-06-20 22:05:12 +0900

    コメントアウトを付けた

--- app/database.py
+++ app/database.py
@@ -7,21 +7,28 @@ from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
 
 from app import const
 
+# データベースのURL
 DATABASE_URL = f"mysql+pymysql://{const.DB_USER}:{const.DB_PASS}@{const.DB_HOST}/{const.DB_NAME}?charset=utf8"
 
+# SQLAlchemyでDB接続するための設定
+# engine はDB接続のインターフェースとして使われる
 engine = create_engine(
     DATABASE_URL,
-    echo=False,
+    echo=False, # SQLのコンソールを非表示
 )
 
+# セッション管理を行う？
+# セッションのファクトリー関数(？？？？？)
 SessionLocal = scoped_session(
+    # セッションの作成
     sessionmaker(
         autocommit=False,
         autoflush=False,
-        bind=engine,
+        bind=engine, # engineを接続
     ),
 )
 
+# テーブル定義の基底クラス
 Base = declarative_base()
 
 
--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,14 @@
 from .database import SessionLocal
 
-
+# DBのセッションを作る関数？
+# これは同期する。asyncで非同期で返すのも作れるらしい。
+# SQLAlchemy だと SessionLocal() を使うためこの形式
 def get_db():
+    # データベースのセッションを生成する
     db = SessionLocal()
     try:
-        yield db
-    finally:
-        db.close()
+        # yield: 値を一時的に返す（関数はいったん、停止する）
+        yield db # セッションを返す
+        
+    finally: # 処理が戻ってきたら（リクエスト処理が終わったら）
+        db.close() # セッションを閉じて解放
